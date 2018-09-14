37c4b2380c8d6aa15c08e8af4a8dd20c3f31c4fc
Mattias Svedklint <m.svedklint@telia.com>
2018-09-14 09:27:49 +0200

    Add sort to table

--- lab-1/src/components/table/table.js
+++ lab-1/src/components/table/table.js
@@ -2,20 +2,33 @@ import React, { Component } from "react";
 import "./table.scss";
 import PropTypes from 'prop-types';
 
+let rowToSort = '';
+
 class Table extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: this.props.users,
+            desc: false,
+        };
+        this.sortNumber = this.sortNumber.bind(this);
+        this.sortString = this.sortString.bind(this);
+    }
 
     render() {
         return (<table className="table">
             <tbody>
-            <tr>
-                <th>Name</th>
-                <th>Surname</th>
-                <th>Age</th>
-                <th>Phone</th>
-                <th>Gender</th>
+            <tr onClick={(element) => {this.headerClick(element.target);}}>
+                <th direction="desc">Name</th>
+                <th direction="desc">Surname</th>
+                <th direction="desc">Age</th>
+                <th direction="desc">Phone</th>
+                <th direction="desc">Gender</th>
             </tr>
             {
-                this.props.users.map((user, i) => {
+                this.state.users.map((user, i) => {
                     return this.createRow(user, i);
                 })
             }
@@ -25,7 +38,7 @@ class Table extends Component {
 
     createRow(user, i) {
         return (
-            <tr key={i} onClick={() => {this.tableClick(user ,i);}}>
+            <tr key={i} onClick={() => {this.rowClick(user ,i);}}>
                 <td>{user.name}</td>
                 <td>{user.surname}</td>
                 <td>{user.age}</td>
@@ -35,11 +48,67 @@ class Table extends Component {
         )
     }
 
-    tableClick(item, i) {
+    rowClick(item, i) {
         this.props.tableClick(item, i)
     }
 
+    headerClick(item) {
+        if (item.getAttribute('direction') === 'desc') {
+            item.setAttribute('direction', 'asc');
+            this.setState({
+                desc: true
+            })
+        } else {
+            item.setAttribute('direction', 'desc');
+            this.setState({
+                desc: false
+            })
+        }
 
+        rowToSort = item.innerHTML.toLowerCase();
+        if(item.innerHTML === 'Age' || item.innerHTML === 'Phone') {
+            this.state.users.sort(this.sortNumber);
+        } else {
+            this.state.users.sort(this.sortString);
+        }
+
+
+
+        this.setState({
+            users: this.state.users
+        });
+
+    }
+
+
+    sortNumber(a, b) {
+
+        if(this.state.desc) {
+            if (Number(a[rowToSort]) < Number(b[rowToSort])) {
+                return -1;
+            }
+            return 1;
+        } else {
+            if (Number(a[rowToSort]) < Number(b[rowToSort])) {
+                return 1;
+            }
+            return -1;
+        }
+    }
+
+    sortString(a, b) {
+        if(this.state.desc) {
+            if (a[rowToSort] < b[rowToSort]) {
+                return -1;
+            }
+            return 1;
+        } else {
+            if (a[rowToSort] < b[rowToSort]) {
+                return 1;
+            }
+            return -1;
+        }
+    }
 }
 
 Table.propTypes = {
