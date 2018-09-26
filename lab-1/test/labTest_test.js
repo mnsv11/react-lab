1654ce4287732e054d3e11e3a1f5a2d553f34594
Mattias Svedklint <m.svedklint@telia.com>
2018-09-26 15:35:35 +0200

    Update Puppeteer test

--- lab-1/test/labTest_test.js
+++ lab-1/test/labTest_test.js
@@ -10,10 +10,10 @@ Scenario('check Welcome page on site', (I) => {
     I.see('Users');
 });
 
-Scenario('Add new user', async (I) => {
+Scenario('Validate errors and add user', async (I) => {
     I.click('Add new user');
 
-    I.fillField({name: 'name'}, 'Simon');
+    I.fillField({name: 'name'}, 'A');
     I.fillField({name: 'last name'}, 'Ericsson');
     I.fillField({name: 'age'}, '99');
     I.selectOption('select[name=gender]', 'Male');
@@ -24,6 +24,55 @@ Scenario('Add new user', async (I) => {
     I.fillField({name: 'country'}, 'Sweden');
     I.fillField({name: 'phone'}, '078945612');
 
+    I.click('Save');
+    I.see('Require at least 3 characters');
+
+    I.fillField({name: 'last name'}, 'B');
+    I.fillField({name: 'name'}, 'Simon');
+    I.click('Save');
+    I.see('Require at least 3 characters');
+
+    I.fillField({name: 'last name'}, 'Ericsson');
+    I.fillField({name: 'age'}, '0');
+    I.click('Save');
+    I.see('Age must be larger then 0');
+
+    I.fillField({name: 'age'}, '99');
+    I.selectOption('select[name=gender]', '');
+    I.click('Save');
+    I.see('Require at least 3 characters');
+
+    I.selectOption('select[name=gender]', 'Male');
+    I.fillField({name: 'street'}, 'C');
+    I.click('Save');
+    I.see('Require at least 3 characters');
+
+    I.fillField({name: 'street'}, 'Ronnebygatan');
+    I.fillField({name: 'streetNumber'}, '0');
+    I.click('Save');
+    I.see('Street number must be larger then 0');
+
+    I.fillField({name: 'streetNumber'}, '888');
+    I.fillField({name: 'zip'}, 'D');
+    I.click('Save');
+    I.see('Require at least 3 characters');
+
+    I.fillField({name: 'zip'}, '37300');
+    I.fillField({name: 'city'}, 'E');
+    I.click('Save');
+    I.see('Require at least 3 characters');
+
+    I.fillField({name: 'city'}, 'Karlskrona');
+    I.fillField({name: 'country'}, 'F');
+    I.click('Save');
+    I.see('Require at least 3 characters');
+
+    I.fillField({name: 'country'}, 'Sweden');
+    I.fillField({name: 'phone'}, '0');
+    I.click('Save');
+    I.see('Require at least 3 characters');
+
+    I.fillField({name: 'phone'}, '078945612');
     I.click('Save');
 
     I.see('Simon', 'tr');
