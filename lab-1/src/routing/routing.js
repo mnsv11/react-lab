f2cc1fa656f6cb1937f9a5a89ded8b7884739605
Mattias Svedklint <m.svedklint@telia.com>
2018-10-05 11:09:30 +0200

    Add hamburger menu

--- lab-1/src/routing/routing.js
+++ lab-1/src/routing/routing.js
@@ -5,15 +5,27 @@ import Users from '../components/users/users';
 import About from "../components/about/about";
 import ChartPage from "../components/chartPage/chartPage";
 import ExpandPage from "../components/expandPage/expandPage";
+import menu from "../assets/images/menu.png";
 
 class Routing extends Component {
 
+    constructor(props) {
+        super(props);
+        this.displayMenu = this.displayMenu.bind(this);
+        this.hideMenu = this.hideMenu.bind(this);
+    }
 
     render() {
         return (
             <Router>
                 <div className="menu">
                     <div className="menu-links">
+                        <div className='hamburger'>
+                            <img src={menu} alt='' onMouseEnter={this.displayMenu}/>
+                            {
+                                this.dropDownMenu()
+                            }
+                        </div>
                         <MenuLink activeOnlyWhenExact={true} to="/" label="Users" />
                         <MenuLink to="/charts" label="Charts" />
                         <MenuLink to="/expandPage" label="ExpandPage" />
@@ -29,7 +41,41 @@ class Routing extends Component {
             </Router>
         )
     }
+
+
+    displayMenu() {
+        this.refs.dropDownMenu.style.display = 'block';
+    }
+
+    hideMenu() {
+        this.refs.dropDownMenu.style.display = 'none';
+    }
+
+    dropDownMenu() {
+        return (
+            <div className='dropDownMenu' ref='dropDownMenu' onMouseLeave={this.hideMenu}>
+                <MenuLink activeOnlyWhenExact={true} to="/" label="Users" />
+                <MenuLink to="/charts" label="Charts" />
+                <MenuLink to="/expandPage" label="ExpandPage" />
+                <MenuLink to="/about" label="About" />
+            </div>
+        )
+    }
+
 }
+
+/*
+function DropDownMenu() {
+    return (
+        <div className='dropDownMenu'>
+            <MenuLink activeOnlyWhenExact={true} to="/" label="Users" />
+            <MenuLink to="/charts" label="Charts" />
+            <MenuLink to="/expandPage" label="ExpandPage" />
+            <MenuLink to="/about" label="About" />
+        </div>
+    )
+}
+*/
 
 export default Routing;
 
